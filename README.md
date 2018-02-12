2707aabd3c4311c18d10691acaa5bbb19294bf9d
Charles Edge <krypted@me.com>
2018-02-11 23:46:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Atlassian Add-on using Express
+
+Congratulations! You've successfully created an Atlassian Connect Add-on using the Express web application framework.
+
+## What's next?
+
+[Read the docs](https://bitbucket.org/atlassian/atlassian-connect-express/src/master/README.md#markdown-header-install-dependencies).
+
+I've come across a beautiful tutorial which explains along with the screenshots what needs to be done in order to install the addon! Here's the link:
+https://developer.atlassian.com/server/hipchat/getting-started-with-atlassian-connect-express/
+
+Here's what you should do:
+- Start with "Prerequisites" section
+- Skip "Create a new add-on project" section
+- Go through the whole "Start the add-on" section
+- Instead of "Install the add-on in HipChat" section, choose this one
+
+If you don't have "Upload add-on" link in there, click on the "Settings" link under the add-ons list on the "Manage Add-ons" page and check "Enable Private Listings" & "Enable Development Mode" checkboxes and refresh the page
---
+++ routes/index.js
@@ -0,0 +1,84 @@
+module.exports = function (app, addon) {
+
+    // Root route. This route will serve the `atlassian-connect.json` unless the
+    // documentation url inside `atlassian-connect.json` is set
+    app.get('/', function (req, res) {
+        res.format({
+            // If the request content-type is text-html, it will decide which to serve up
+            'text/html': function () {
+                res.redirect('/atlassian-connect.json');
+            },
+            // This logic is here to make sure that the `atlassian-connect.json` is always
+            // served up when requested by the host
+            'application/json': function () {
+                res.redirect('/atlassian-connect.json');
+            }
+        });
+    });
+
+    app.post('/issue_created_webhook', addon.authenticate(), function (req, res) {
+            var serial = req.body.issue.fields.customfield_10024;
+            if (!serial) {
+                return
+            }
+
+            var Client = require('node-rest-client').Client;
+            var auth = {user: "nazar", password: "nazar"};
+            var client = new Client(auth);
+            var params = {
+                headers: {"Content-Type": "application/json"}
+            };
+            client.get("https://kryptedjamf.jamfcloud.com/JSSResource/mobiledevices/match/" + serial, params, function (data) {
+                if (data && data.mobile_devices && data.mobile_devices.size > 0) {
+                    var deviceId = data.mobile_devices.mobile_device.id;
+
+                    if (!deviceId) {
+                        return
+                    }
+
+                    client.get("https://kryptedjamf.jamfcloud.com/JSSResource/mobiledevices/id/" + deviceId, params, function (data) {
+                        if (data && data.mobile_device) {
+                            var options_auth = {user: "devnsobchuk@gmail.com", password: "Li100kNaz"};
+                            client = new Client(options_auth);
+                            var params = {
+                                data: {
+                                    fields: {
+                                        "customfield_10026": JSON.stringify(data.mobile_device)
+                                    }
+                                },
+                                headers: {"Content-Type": "application/json"}
+                            };
+                            client.put(req.body.issue.self, params, function (data, response) {
+                                console.log(data);
+                                console.log(response);
+                            });
+                        }
+                    });
+                }
+            });
+
+        }
+    );
+
+    // Add any additional route handlers you need for views or REST resources here...
+
+    // load any additional files you have in routes and apply those to the app
+    {
+        var Client = require('node-rest-client').Client;
+        var fs = require('fs');
+        var path = require('path');
+        var files = fs.readdirSync("routes");
+        for (var index in files) {
+            var file = files[index];
+            if (file === "index.js") continue;
+            // skip non-javascript files
+            if (path.extname(file) != ".js") continue;
+
+            var routes = require("./" + path.basename(file));
+
+            if (typeof routes === "function") {
+                routes(app, addon);
+            }
+        }
+    }
+};
